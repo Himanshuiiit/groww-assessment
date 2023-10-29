e67d86e63ffe545499596488dab796b7529f7af5
Himanshu Verma <hims6482@gmail.com>
2023-10-29 18:54:18 +0530

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       ) : (
         <>
           <div className="w-full bg-gray-600 -mb-6 py-2 text-center text-lg">
-            Search Result for "{searchString}"
+            Search Result for &ldquo;{searchString}&ldquo;
           </div>
           <Table active={2} searchString={searchString} />
         </>
